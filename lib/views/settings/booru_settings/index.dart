a23271f5f5cfb1cdd05e0f4ac6573c4faba3574b
abUwUser <resucutie@proton.me>
2024-02-18 19:24:41 -0300

    add syncthing promo to make people life's easier

--- lib/views/settings/booru_settings/index.dart
+++ lib/views/settings/booru_settings/index.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:go_router/go_router.dart';
 import 'package:localbooru/api/index.dart';
 import 'package:localbooru/components/headers.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class BooruSettings extends StatefulWidget {
     const BooruSettings({super.key, required this.prefs, required this.booru});
@@ -27,6 +29,14 @@ class _BooruSettingsState extends State<BooruSettings> {
                     onTap: () => context.push("/settings/booru/tag_types"),
                 ),
                 const Divider(),
+                const SmallThemedHeader("Other"),
+                ListTile(
+                    title: const Text("Syncing"),
+                    subtitle: const Text("This program does not offer syncing capabilities out of the box, but if you want to sync your computer storage, we recommend using Syncthing"),
+                    leading: SvgPicture.asset("assets/syncthing.svg", width: 24, height: 24,),
+                    trailing: const Icon(Icons.open_in_new),
+                    onTap: () => launchUrlString("https://syncthing.net/"),
+                ),
                 ListTile(
                     subtitle: Text("Current booru path: ${widget.booru.path}")
                 ),
