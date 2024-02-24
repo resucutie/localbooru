421d1ea0234736b2a078da155d4c5e4ed0b52cf4
abUwUser <resucutie@proton.me>
2024-02-24 15:43:52 -0300

    add file type info

--- lib/components/image_display.dart
+++ lib/components/image_display.dart
@@ -1,4 +1,6 @@
+import 'dart:io';
 import 'dart:typed_data';
+import 'dart:ui';
 
 import 'package:flutter/material.dart';
 import 'package:localbooru/api/index.dart';
@@ -15,17 +17,11 @@ class SilverRepoGrid extends StatelessWidget {
     final Function(BooruImage image)? onPressed;
     final int? autoadjustColumns;
 
-    Future<Uint8List> getVideoPreview(String videoPath) async {
-        if(!lookupMimeType(p.basename(videoPath))!.startsWith("video/")) throw "Not a video";
-
-        final player = Player();
-        final controller = VideoController(player); // has to be created according to https://github.com/media-kit/media-kit/issues/419#issuecomment-1703855470
-        await player.open(Media(videoPath), play: false);
-        await controller.waitUntilFirstFrameRendered;
-        await Future.delayed(const Duration(seconds: 1));
-        await player.seek(Duration.zero); 
-        final bytes = await player.screenshot();
-        return bytes!;
+    String? getType(String filename) {
+        final mime = lookupMimeType(filename)!;
+        if(mime.startsWith("video")) return "video";
+        if(mime.startsWith("image/gif")) return "gif";
+        return "image";
     }
   
     @override
@@ -50,30 +46,88 @@ class SilverRepoGrid extends StatelessWidget {
                 ),
                 delegate: SliverChildListDelegate(images.map((image) {
                     return Padding(
-                        padding: const EdgeInsets.all(5.0),
+                        padding: const EdgeInsets.all(4.0),
                         child: MouseRegion(
                             cursor: SystemMouseCursors.click,
                             child: GestureDetector(
                                 onTap: () {if(onPressed != null) onPressed!(image);},
-                                child: FutureBuilder(
-                                    future: getVideoPreview(image.path),
-                                    builder: (context, snapshot) {
-                                        if(snapshot.hasData || snapshot.hasError) {
-                                            return Image(
-                                                image: !snapshot.hasError
-                                                    ? MemoryImage(snapshot.data!)
-                                                    : FileImage(image.getImage()) as ImageProvider, 
-                                                fit: BoxFit.cover
-                                            );
-                                        }
-                                        if(snapshot.hasError && snapshot.error != "Not a video") throw snapshot.error!;
-                                        return const Center(child: CircularProgressIndicator(),);
-                                    })
-                            ),
+                                child: Stack(
+                                    children: [
+                                        AspectRatio(aspectRatio: 1, child: GridImage(path: image.path,),),
+                                        if(getType(image.filename) != "image") Positioned(
+                                            // top:6,
+                                            // left:6,
+                                            child: Container(
+                                                decoration: const BoxDecoration(
+                                                    color: Color.fromARGB(60, 0, 0, 0),
+                                                    borderRadius: BorderRadius.only(
+                                                        bottomRight: Radius.circular(8),
+                                                    )
+                                                ),
+                                                padding: const EdgeInsets.symmetric(horizontal: 6, vertical: 2),
+                                                child: Text(getType(image.filename)!.toUpperCase(),
+                                                    style: const TextStyle(
+                                                        fontSize: 12
+                                                    ),
+                                                ),
+                                            ),
+                                        ),
+                                    ],
+                                ),
+                            )
                         ),
                     );
                 }).toList()),
             );
         }
+    }
+}
+
+class GridImage extends StatefulWidget {
+    const GridImage({super.key, required this.path});
+
+    final String path;
+
+    @override
+    State<GridImage> createState() => _GridImageState();
+}
+
+class _GridImageState extends State<GridImage> {
+    final _player = Player();
+    
+    Future<Uint8List> getVideoPreview(String videoPath) async {
+        if(!lookupMimeType(p.basename(videoPath))!.startsWith("video/")) throw "Not a video";
+
+        final controller = VideoController(_player); // has to be created according to https://github.com/media-kit/media-kit/issues/419#issuecomment-1703855470
+        await _player.open(Media(videoPath), play: false);
+        await controller.waitUntilFirstFrameRendered;
+        await Future.delayed(const Duration(seconds: 1)); // idk why but this works
+        await _player.seek(Duration.zero); 
+        final bytes = await _player.screenshot();
+        return bytes!;
+    }
+
+    @override
+    void dispose() {
+        _player.dispose();
+        super.dispose();
+    }
+
+    @override
+    Widget build(context) {
+        return FutureBuilder(
+            future: getVideoPreview(widget.path),
+            builder: (context, snapshot) {
+                if(snapshot.hasData || snapshot.hasError) {
+                    return Image(
+                        image: !snapshot.hasError
+                            ? MemoryImage(snapshot.data!)
+                            : FileImage(File(widget.path)) as ImageProvider, 
+                        fit: BoxFit.cover,
+                    );
+                }
+                return const Center(child: CircularProgressIndicator(),);
+            }
+        );
     }
 }
