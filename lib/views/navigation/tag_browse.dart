dea7f4579b66f3ecf1cafa73e9bb9ce644bb9818
abUwUser <resucutie@proton.me>
2024-03-26 20:46:44 -0300

    page indicator improvements

--- lib/views/navigation/tag_browse.dart
+++ lib/views/navigation/tag_browse.dart
@@ -1,11 +1,13 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:go_router/go_router.dart';
 import 'package:localbooru/api/index.dart';
 import 'package:localbooru/components/image_display.dart';
 import 'package:localbooru/utils/constants.dart';
 import 'package:localbooru/views/navigation/home.dart';
+import 'package:scrollable_positioned_list/scrollable_positioned_list.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class GalleryViewer extends StatefulWidget {
@@ -134,31 +136,46 @@ class SearchBarHeaderDelegate extends SliverPersistentHeaderDelegate {
     bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) => false;
 }
 
-class PageDisplay extends StatelessWidget {
+class PageDisplay extends StatefulWidget {
+    const PageDisplay({super.key, this.height = 48.0, required this.currentPage, required this.pages, this.onSelect});
     final double height;
     final int pages;
     final int currentPage;
     final Function(int selectedPage)? onSelect;
 
-    const PageDisplay({super.key, this.height = 48.0, required this.currentPage, required this.pages, this.onSelect});
+    @override
+    State<PageDisplay> createState() => _PageDisplayState();
+}
+
+class _PageDisplayState extends State<PageDisplay> {
+    final ItemScrollController itemScrollController = ItemScrollController();
+
+    @override
+    void initState() {
+        super.initState();
+        SchedulerBinding.instance.addPostFrameCallback((_) {
+            itemScrollController.jumpTo(index: widget.currentPage, alignment: 0.25);
+        });
+    }
 
     @override
     Widget build(context) {
         return SizedBox(
-            height: height,
+            height: widget.height,
             child: Center(
                 child: ScrollConfiguration(
                     behavior: const MaterialScrollBehavior().copyWith(
                         dragDevices: {PointerDeviceKind.mouse, PointerDeviceKind.touch, PointerDeviceKind.trackpad, PointerDeviceKind.stylus},
                     ),
-                    child: ListView.separated(
-                        itemCount: pages,
+                    child: ScrollablePositionedList.separated(
+                        itemCount: widget.pages,
                         scrollDirection: Axis.horizontal,
                         padding: const EdgeInsets.all(8.0),
                         shrinkWrap: true,
                         separatorBuilder: (context, index) => const SizedBox(width: 8),
+                        itemScrollController: itemScrollController,
                         itemBuilder: (context, index) {
-                            final bool isCurrentPage = currentPage == index;
+                            final bool isCurrentPage = widget.currentPage == index;
                             Widget icon = Text((index + 1).toString());
                             final ButtonStyle buttonStyle = TextButton.styleFrom(
                                 minimumSize: Size.zero,
@@ -167,11 +184,11 @@ class PageDisplay extends StatelessWidget {
 
                             void onPressed() {
                                 if(isCurrentPage) return;
-                                if(onSelect != null) onSelect!(index);
+                                if(widget.onSelect != null) widget.onSelect!(index);
                             }
 
                             return isCurrentPage
-                                ? FilledButton(onPressed: onPressed, style: buttonStyle, child: icon)
+                                ? FilledButton(onPressed: onPressed, style: buttonStyle, child: icon,)
                                 : OutlinedButton(onPressed: onPressed, style: buttonStyle, child: icon);
                         }
                     ),
