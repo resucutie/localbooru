2fc44086b8037afeb86852c1b36e58d51addec05
abUwUser <resucutie@proton.me>
2024-06-21 23:10:48 -0300

    image_manager: properly name the bulk add tool and make it to return to the first page when going back

--- lib/views/image_manager/shell.dart
+++ lib/views/image_manager/shell.dart
@@ -48,10 +48,7 @@ class _ImageManagerShellState extends State<ImageManagerShell> {
         final futureImageIDs = presets.asMap().keys.map((e) => "${e + listLength}").toList();
         
         for (final (index, preset) in presets.indexed) {
-            debugPrint("isCorelated $isCorelated");
-            debugPrint("presets.length <= 1 ${presets.length <= 1}");
             if(isCorelated && presets.length > 1) {
-                debugPrint("isCorelated was ran");
                 preset.relatedImages = futureImageIDs.where((e) => e != futureImageIDs[index]).toList();
             }
 
@@ -62,106 +59,128 @@ class _ImageManagerShellState extends State<ImageManagerShell> {
         if(context.mounted) context.pop();
     }
 
+    String generateName(PresetImage preset) {
+        return preset.image != null ? p.basenameWithoutExtension(preset.image!.path) : "Image ${presets.indexOf(preset) + 1}";
+    }
+
     @override
     Widget build(BuildContext context) {
-        return Scaffold(
-            key: _scaffoldKey,
-            appBar: AppBar(
-                // title: Text("${isEditing ? "Edit" : "Add"} image"),
-                title: const Text("Add Image"),
-                actions: [
-                    IconButton(
-                        icon: const Icon(Icons.library_add),
-                        tooltip: "Collections",
-                        onPressed: () => _scaffoldKey.currentState!.openEndDrawer(),
-                    ),
-                    TextButton.icon(
-                        icon: const Icon(Icons.check),
-                        label: const Text("Done"),
-
-                        onPressed: !isSaving && !hasError ? saveImages : null
-                    ),
-                ],
-                bottom: isSaving ? AppBarLinearProgressIndicator(value: savedImages / presets.length,) : null,
-            ),
-            endDrawer: Drawer(
-                child: SingleChildScrollView(
-                    child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                            const Padding(
-                                padding: EdgeInsets.all(16),
-                                child: Text("Collections"),
+        return OrientationBuilder(
+            builder: (context, orientation) => Scaffold(
+                key: _scaffoldKey,
+                appBar: AppBar(
+                    title: imagePage >= 0 ? ListTile(
+                        contentPadding: EdgeInsets.zero,
+                        title: Text("${presets[imagePage].replaceID != null ? "Edit" : "Add"} image", style: const TextStyle(fontSize: 20.0), overflow: TextOverflow.ellipsis),
+                        subtitle: Text(generateName(presets[imagePage]), style: const TextStyle(fontSize: 14.0), overflow: TextOverflow.ellipsis),
+                    ) : const Text("Manage images"),
+                    actions: [
+                        IconButton(
+                            icon: Badge(
+                                label: Text("${presets.length}"),
+                                offset: const Offset(7, -7),
+                                isLabelVisible: presets.length > 1,
+                                child: const Icon(Icons.library_add)
                             ),
-                            ListTile(
-                                title: const Text("Manage collections"),
-                                textColor: imagePage == -1 ? Theme.of(context).colorScheme.primary : null,
-                                iconColor: imagePage == -1 ? Theme.of(context).colorScheme.primary : null,
-                                leading: const Icon(Icons.photo_library),
-                                onTap: imagePage == -1 ? null : () {
-                                    setState(() => imagePage = -1);
-                                    _scaffoldKey.currentState!.closeEndDrawer();
-                                },
-                            ),
-                            const Divider(),
-                            ...List.generate(presets.length, (index) => ListTile(
-                                title: Text(presets[index].image != null ? p.basenameWithoutExtension(presets[index].image!.path) : "Image ${index + 1}"),
-                                leading: const Icon(Icons.image_outlined),
-                                textColor: imagePage == index ? Theme.of(context).colorScheme.primary : null,
-                                iconColor: imagePage == index ? Theme.of(context).colorScheme.primary : null,
-                                onTap: imagePage == index ? null : () {
-                                    setState(() => imagePage = index);
-                                    _scaffoldKey.currentState!.closeEndDrawer();
-                                },
-                                trailing: presets.length == 1 ? null : IconButton(
-                                    tooltip: "Delete",
-                                    icon: const Icon(Icons.delete),
-                                    onPressed: () {
-                                        presets.removeAt(index);
-                                        setState(() => imagePage = imagePage > index ? imagePage - 1 : imagePage);
+                            tooltip: "Add images in bulk",
+                            onPressed: () => _scaffoldKey.currentState!.openEndDrawer(),
+                        ),
+                        if(orientation == Orientation.portrait) IconButton(
+                            icon: const Icon(Icons.check),
+                            onPressed: !isSaving && !hasError ? saveImages : null
+                        ) else TextButton.icon(
+                            icon: const Icon(Icons.check),
+                            label: const Text("Done"),
+                            onPressed: !isSaving && !hasError ? saveImages : null
+                        ),
+                    ],
+                    bottom: isSaving ? AppBarLinearProgressIndicator(value: savedImages / presets.length,) : null,
+                ),
+                endDrawer: Drawer(
+                    child: SingleChildScrollView(
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                                const Padding(
+                                    padding: EdgeInsets.all(16),
+                                    child: Text("Bulk Adding"),
+                                ),
+                                ListTile(
+                                    title: const Text("Manage images"),
+                                    textColor: imagePage == -1 ? Theme.of(context).colorScheme.primary : null,
+                                    iconColor: imagePage == -1 ? Theme.of(context).colorScheme.primary : null,
+                                    leading: const Icon(Icons.photo_library),
+                                    onTap: imagePage == -1 ? null : () {
+                                        setState(() => imagePage = -1);
+                                        _scaffoldKey.currentState!.closeEndDrawer();
                                     },
                                 ),
-                            )),
-                            const Divider(),
-                            ListTile(
-                                title: const Text("Add image"),
-                                leading: const Icon(Icons.add),
-                                onTap: () async {
-                                    FilePickerResult? result = await FilePicker.platform.pickFiles(type: FileType.media, allowMultiple: true);
-                                    if (result != null) {
-                                        for (PlatformFile file in result.files) {
-                                            if(file.path != null) presets.add(PresetImage(image: File(file.path!), uniqueKey: UniqueKey()));
-                                        }
-                                        setState(() => imagePage = presets.length - 1);
+                                const Divider(),
+                                ...List.generate(presets.length, (index) => ListTile(
+                                    title: Text(generateName(presets[index])),
+                                    leading: const Icon(Icons.image_outlined),
+                                    textColor: imagePage == index ? Theme.of(context).colorScheme.primary : null,
+                                    iconColor: imagePage == index ? Theme.of(context).colorScheme.primary : null,
+                                    onTap: imagePage == index ? null : () {
+                                        setState(() => imagePage = index);
                                         _scaffoldKey.currentState!.closeEndDrawer();
+                                    },
+                                    trailing: presets.length == 1 ? null : IconButton(
+                                        tooltip: "Remove",
+                                        icon: const Icon(Icons.close),
+                                        onPressed: () {
+                                            presets.removeAt(index);
+                                            setState(() => imagePage = imagePage >= index ? imagePage - 1 : imagePage);
+                                        },
+                                    ),
+                                )),
+                                const Divider(),
+                                ListTile(
+                                    title: const Text("Add image"),
+                                    leading: const Icon(Icons.add),
+                                    onTap: () async {
+                                        FilePickerResult? result = await FilePicker.platform.pickFiles(type: FileType.media, allowMultiple: true);
+                                        if (result != null) {
+                                            if(presets.first.image == null && result.files.length > 1) presets = [];
+                                            for (PlatformFile file in result.files) {
+                                                if(file.path != null) presets.add(PresetImage(image: File(file.path!), uniqueKey: UniqueKey()));
+                                            }
+                                            setState(() => imagePage = presets.length - 1);
+                                            _scaffoldKey.currentState!.closeEndDrawer();
+                                        }
                                     }
-                                }
+                                ),
+                            ],
+                        ),
+                    ),
+                ),
+                body: PopScope(
+                    canPop: false,
+                    onPopInvoked: (didPop) {
+                        if(didPop) return;
+                        if(imagePage == 0) {
+                            context.pop();
+                        } else {
+                            setState(() => imagePage = 0);
+                        }
+                    },
+                    child: IndexedStack(
+                        index: imagePage + 1,
+                        children: [
+                            GeneralCollectionManagerScreen(
+                                corelated: isCorelated,
+                                onCorelatedChanged: (value) => setState(() => isCorelated = value),
                             ),
+                            for (final (index, preset) in presets.indexed) ImageManagerForm(
+                                key: preset.uniqueKey, // replace index by something else
+                                preset: preset,
+                                onChanged: (preset) => setState(() => presets[index] = preset),
+                                onErrorUpdate: (containsError) => setState(() => hasError = containsError),
+                            )
                         ],
                     ),
                 ),
-            ),
-            body: IndexedStack(
-                index: imagePage + 1,
-                children: [
-                    GeneralCollectionManagerScreen(
-                        corelated: isCorelated,
-                        onCorelatedChanged: (value) => setState(() => isCorelated = value),
-                    ),
-                    for (final (index, preset) in presets.indexed) (() {
-                        // debugPrint("${presets.map((e) => e.image).toList()}");
-                        return ImageManagerForm(
-                            key: preset.uniqueKey, // replace index by something else
-                            preset: preset,
-                            onChanged: (preset) => setState(() => presets[index] = preset),
-                            onErrorUpdate: (containsError) {
-                                setState(() => hasError = containsError);
-                                debugPrint("$hasError");
-                            },
-                        );
-                    })()
-                ],
-            ),
+            )
         );
     }
 }
