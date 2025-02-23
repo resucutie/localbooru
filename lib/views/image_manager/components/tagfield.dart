7c96a01509869958ae96609ccdace53de703f352
resucutie <resucutie@proton.me>
2025-02-23 16:30:39 -0300

    image_manager: implement counter on tagfield and make suggestions list scrollable

--- lib/views/image_manager/components/tagfield.dart
+++ lib/views/image_manager/components/tagfield.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
 import 'package:localbooru/api/index.dart';
+import 'package:localbooru/api/tags/index.dart';
 
 class TagField extends StatefulWidget {
     const TagField({super.key, this.controller,  this.decoration, this.validator, this.style, this.type = "generic", this.onChanged});
@@ -20,7 +22,7 @@ class _TagFieldState extends State<TagField> {
     late TextEditingController controller;
     GlobalKey textboxKey = GlobalKey();
 
-    List<String>? allTags;
+    List<BooruTagCounterDisplay<NormalTag>> allTags = [];
 
     @override
     void initState() {
@@ -32,8 +34,7 @@ class _TagFieldState extends State<TagField> {
     Future<void> cacheTags() async {
         Booru currentBooru = await getCurrentBooru();
         //todo: change behavior to support BooruTagCounterDisplay
-        allTags = (await currentBooru.getAllSavedTagsFromType(widget.type)).map((e) => e.tag.text,).toList();
-        debugPrint("$allTags");
+        allTags = await currentBooru.getAllSavedTagsFromType(widget.type);
     }
 
     bool spawnAtBottom() {
@@ -45,74 +46,89 @@ class _TagFieldState extends State<TagField> {
 
     @override
     Widget build(context) {
-        return RawAutocomplete<String>(
-            textEditingController: controller,
-            focusNode: _focusNode,
-            optionsBuilder: (textEditingValue) async {
-                if (textEditingValue.text == '') {
-                    return const Iterable<String>.empty();
-                } else {
-                    List<String> tagList = textEditingValue.text.split(" ");
-                    List<String> restOfList = List.from(tagList);
-                    restOfList.removeLast();
-                    String tag = tagList.last;
-
-                    if(allTags == null) await cacheTags();
-
-                    List<String> matches = List<String>.from(allTags!);
-
-                    matches.retainWhere((s){
-                        return s.contains(tag) && !restOfList.contains(s) && s != tag;
-                    });
-                    return matches.map((e) => restOfList.isEmpty ? e : "${restOfList.join(" ")} $e").toList();
-                }
-            },
-            optionsViewBuilder: (context, onSelected, options) {
-                int highlightedIndex = AutocompleteHighlightedOption.of(context);
-                return Align(
-                    alignment: spawnAtBottom() ? Alignment.topCenter : Alignment.bottomCenter,
-                    child: Material(
-                        elevation: 4.0,
-                        child: Container(
-                            constraints: const BoxConstraints(maxHeight: 300),
-                            alignment: Alignment.bottomCenter,
-                            child: ListView.builder(
-                                itemCount: options.length,
-                                itemBuilder: (context, index) {
-                                    final currentOption = options.elementAt(index);
-                                    return ListTile(
-                                        title: Text(currentOption.split(" ").last),
-                                        onTap: () => onSelected(currentOption),
-                                        selected: highlightedIndex == index,
-                                        selectedColor: widget.style?.color,
-                                        selectedTileColor: widget.style?.color?.withOpacity(0.1),
-                                    );
-                                },
-                            ),
-                        )
-                    ),
-                );
-            },
-            optionsViewOpenDirection: spawnAtBottom() ? OptionsViewOpenDirection.down : OptionsViewOpenDirection.up,
-            fieldViewBuilder: (context, textController, focusNode, onFieldSubmitted) {
-                return TextFormField(
-                    key: textboxKey,
-                    controller: textController,
-                    focusNode: focusNode,
-                    decoration: widget.decoration,
-                    keyboardType: TextInputType.text,
-                    minLines: 1,
-                    maxLines: 6,
-                    inputFormatters: [FilteringTextInputFormatter.deny(RegExp(r'\n')),],
-                    validator: widget.validator,
-                    style: widget.style,
-                    onFieldSubmitted: (value) {
-                        debugPrint(value);
-                        onFieldSubmitted();
+        return LayoutBuilder(
+            builder: (context, constrains) {
+                return RawAutocomplete<BooruTagCounterDisplay<NormalTag>>(
+                    textEditingController: controller,
+                    focusNode: _focusNode,
+                    optionsBuilder: (textEditingValue) async {
+                        if (textEditingValue.text == '') {
+                            return const Iterable<BooruTagCounterDisplay<NormalTag>>.empty();
+                        } else {
+                            List<String> tagList = textEditingValue.text.split(" ");
+                            List<String> restOfList = List.from(tagList);
+                            restOfList.removeLast();
+                            String tagToSearch = tagList.last;
+                
+                            if(allTags.isEmpty) await cacheTags();
+                
+                            List<BooruTagCounterDisplay<NormalTag>> matches = List<BooruTagCounterDisplay<NormalTag>>.from(allTags);
+                
+                            matches.retainWhere((s){
+                                return s.tag.text.contains(tagToSearch) && !restOfList.contains(s.tag.text) && s.tag.text != tagToSearch;
+                            });
+                            return matches;
+                        }
                     },
-                    onChanged: widget.onChanged,
+                    optionsViewBuilder: (context, onSelected, options) {
+                        return Align(
+                            alignment: spawnAtBottom() ? Alignment.topLeft : Alignment.bottomLeft,
+                            child: Material(
+                                elevation: 4.0,
+                                child: ConstrainedBox(
+                                    constraints: BoxConstraints(maxHeight: 300, maxWidth: constrains.maxWidth),
+                                    child: ListView.builder(
+                                        itemCount: options.length,
+                                        shrinkWrap: true,
+                                        padding: EdgeInsets.zero,
+                                        itemBuilder: (context, index) => Builder(
+                                            builder: (context) {
+                                                final shouldHighlight = AutocompleteHighlightedOption.of(context) == index;
+                                                if (shouldHighlight) {
+                                                    SchedulerBinding.instance.addPostFrameCallback((Duration timeStamp) {
+                                                        Scrollable.ensureVisible(context, alignment: 0.5, duration: Duration(milliseconds: 100));
+                                                    });
+                                                }
+                                                final currentOption = options.elementAt(index);
+                                                return ListTile(
+                                                    title: Text(currentOption.tag.text),
+                                                    onTap: () => onSelected(currentOption),
+                                                    selected: shouldHighlight,
+                                                    selectedColor: widget.style?.color,
+                                                    selectedTileColor: widget.style?.color?.withValues(alpha: 0.1),
+                    
+                                                    trailing: Text("${currentOption.callQuantity}"),
+                                                );
+                                            },
+                                        ),
+                                    ),
+                                )
+                            ),
+                        );
+                    },
+                    displayStringForOption: (option) => "${option.tag.text} ",
+                    optionsViewOpenDirection: spawnAtBottom() ? OptionsViewOpenDirection.down : OptionsViewOpenDirection.up,
+                    fieldViewBuilder: (context, textController, focusNode, onFieldSubmitted) {
+                        return TextFormField(
+                            key: textboxKey,
+                            controller: textController,
+                            focusNode: focusNode,
+                            decoration: widget.decoration,
+                            keyboardType: TextInputType.text,
+                            minLines: 1,
+                            maxLines: 6,
+                            inputFormatters: [FilteringTextInputFormatter.deny(RegExp(r'\n')),],
+                            validator: widget.validator,
+                            style: widget.style,
+                            onFieldSubmitted: (value) {
+                                debugPrint(value);
+                                onFieldSubmitted();
+                            },
+                            onChanged: widget.onChanged,
+                        );
+                    },
                 );
-            },
+            }
         );
     }
 }
